6b0c46e5ffdd3b30ae3d0794170df6c5cc92f3bf
DavidWagner17 <david_gui2k6@hotmail.com>
2019-04-28 21:48:36 +0000

    Added gems

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,10 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+gem 'jquery-rails'
+gem 'popper_js', '~> 1.11.1'
+gem 'bootstrap', '4.0.0.alpha6'
+
+source 'https://rails=assets.org' do
+  gem 'rails-assets-tether', '>= 1.3.3'  
+end
